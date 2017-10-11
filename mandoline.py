e36c2b08ad3739a601efa5871cd4cc30023b8794
Jon <jon@emissaryinc.com>
2017-10-10 21:52:34 -0700

    working uploads, with randomized hashtags

--- mandoline.py
+++ mandoline.py
@@ -1,19 +1,70 @@
 #!usr/bin/python
+import argparse
 import csv
+import random
+from subprocess import call
 
+# CSV column definitions
+FILENAME = 0
+TIMESTART = 1
+TIMESTOP = 2
+POSTED = 3
+
+def buildHashtags():
+  possibles = ["#trains", "#train", "#training", "#trainspotting",
+      "#slowtrainkeepsonrolling", "#norway", "#choochoo",
+      "#railroads", "#rail", "#trainstation", "#locomotive",
+      "#clouds", "#outside"]
+  random.shuffle(possibles)
+  used = possibles[:5]
+  return " ".join(used)
+
+# Build a comment based on randomized hashtags and templates
+def buildComment(segment):
+  return "On my way across Norway! Wheeee %s" % buildHashtags()
+
+
+next_to_upload_id = 0
 segments = []
+parser = argparse.ArgumentParser(description='Upload segments of video to Instagram')
+parser.add_argument('--basepath', dest='path', default="./",
+    help="The absolute path (ending in /) to a directory containing segment files and a segment list from ffmpeg")
+args = parser.parse_args()
 
 # Read in segments DB
 # Format: filename, start time(sec), end time(sec), posted (0/1)
-with open('segments.db', 'rb') as segmentfile:
+with open("%s%s" % (args.path, 'segments.db'), 'rb') as segmentfile:
   segmentreader = csv.reader(segmentfile)
   for row in segmentreader:
     #row.append(0)  # Add "posted" row, starting with none posted  
+    #print row
     segments.append(row)
-    print row
+
+# Find first that hasn't been posted.
+i = 0
+for row in segments:
+  if row[POSTED] == "0":
+    next_to_upload_id = i
+    break
+  i += 1
+
+# Post to IG
+username = 'iamaslowtrain'
+password = 'Tr4inst4gr4m30'
+filename = "%s%s" % (args.path, segments[next_to_upload_id][FILENAME])
+comment = buildComment(segments[next_to_upload_id])
+print "Posting %s..." % filename
+print "Comment: %s" % comment
+command = "php %s../upload.php %s %s %s \"%s\"" % (
+    args.path, username, password, filename, comment)
+print "Calling command %s" % command
+call(command, shell=True)
+
+segments[next_to_upload_id][POSTED] = 1
 
 # Write segment DB to disk
-with open('segments.db', 'wb') as segmentdb_file:
+with open("%s%s" % (args.path, 'segments.db'), 'wb') as segmentdb_file:
   segmentwriter = csv.writer(segmentdb_file)
   for row in segments:
     segmentwriter.writerow(row)
+
